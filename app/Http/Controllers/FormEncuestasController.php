50d3c46c54c51a4c6427b81b59f8476039d9ecc7
wisin007castro <castrow666@gmail.com>
2020-04-27 17:15:21 -0400

    rutas actualizar y guardar cambiadas

--- app/Http/Controllers/FormEncuestasController.php
+++ app/Http/Controllers/FormEncuestasController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use DateTime;
+
 class FormEncuestasController extends Controller
 {
     public function home_encuestas(){
@@ -27,11 +29,19 @@ class FormEncuestasController extends Controller
         return view("listados.encuesta.listado_densidad", compact('datos'));
     }
 
+    public function listado_preparacion(){
+        $datos = \DB::table('enc_preparaciones')->orderBy('id_preparacion', 'desc')->get();
+        return view("listados.encuesta.listado_preparacion", compact('datos'));
+    }
+
 
 
     //FORMS AGREGAR
     public function form_densidad_agregar(){
         return view("formularios.encuestas.form_densidad_agregar");
+    }
+    public function form_preparacion_agregar(){
+        return view("formularios.encuestas.form_preparacion_agregar");
     }
 
 
@@ -45,6 +55,27 @@ class FormEncuestasController extends Controller
         public function form_densidad_guardar(Request $request){
             dd($request);
             // return view("formularios.encuestas.form_densidad_agregar");
+        }
+
+        public function preparacion_guardar(Request $request){
+            $tiempo_actual = new DateTime(date('Y-m-d H:i:s'));
+            if($request->quema == 1){
+                $con_quema = 1;
+                $sin_quema = 0;
+
+            } else{
+                $con_quema = 0;
+                $sin_quema = 1;
+            }
+            \DB::table('votos_presidenciales')->insert([
+                ['object_id' => 1,
+                 'fecha' => $request->fecha,
+                 'con_quema' => $con_quema,
+                 'sin_quema' => $sin_quema,
+                 'created_at' => $tiempo_actual,
+                 'updated_at' => $tiempo_actual]
+            ]);
+            return redirect('admin/usuario')->with('mensaje', 'Usuario creado con exito');
         }
     
 
